91b0e5ca6f2f5a299d11eb6c80bd25c036351ca0
Fiskhl <kafijaya@student.umn.ac.id>
2023-02-10 14:00:08 +0700

    Penugasan OPREc UMN TV

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}', 'index.html'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    container:{
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        'primary': '#10b981',
+        'secondary': '#64748b',
+        'dark': '#0f172a',
+      },
+      screens:{
+      }
+    },
+  },
+  plugins: [],
+}
